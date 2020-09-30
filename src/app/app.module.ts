4c30444d63777623ca3426a13ae3217f6bc497c5
poornasais <poornasaie@gmail.com>
2020-09-29 20:26:16 -0400

    added footer and pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { Page1Component } from './page1/page1.component';
+import { HeaderComponent } from './header/header.component';
+import { Page2Component } from './page2/page2.component';
+import { Page3Component } from './page3/page3.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Page1Component
+    Page1Component,
+    HeaderComponent,
+    Page2Component,
+    Page3Component,
+    FooterComponent
   ],
   imports: [
     BrowserModule
