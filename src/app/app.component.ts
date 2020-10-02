e2013a910dd644d81d77c2b2791d3900839e3e38
poornasais <poornasaie@gmail.com>
2020-10-01 22:30:04 -0400

    @Input and @ViewChild

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {FooterComponent} from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements  OnInit{
   title = 'SampleAngularApp';
+
+  startDate:string;
+
+  @ViewChild('footer', {static:true})
+  footerComponent:FooterComponent;
+
+  onClickUpdate(){
+
+    this.footerComponent.lastReference = new Date().toString();
+    console.log(this.footerComponent.lastReference);
+  }
+
+  ngOnInit(): void {
+    this.startDate = new Date().toString();
+  }
+
+
 }
--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-footer',
@@ -9,7 +9,11 @@ export class FooterComponent implements OnInit {
 
   constructor() { }
 
+  @Input()
+  lastReference = '';
+
   ngOnInit(): void {
   }
+
 
 }
